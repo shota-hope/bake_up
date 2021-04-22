d5f144fc90d966a8554da9f3919523e25f304109
ohno <ddeeppaa.gogo@gmail.com>
2021-04-22 21:52:12 +0900

    簡易版ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/show'
-  post '/pages/guest_sign_in', to: 'homes#new_guest'
+  post '/pages/guest_sign_in', to: 'pages#new_guest'
 
 end
