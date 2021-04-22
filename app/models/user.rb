5b3f7e22f98cc35d382e960971b839825a328ee0
ohno <ddeeppaa.gogo@gmail.com>
2021-04-23 00:14:24 +0900

    バリデーション追加、User/Postの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
