434bb1871f633d6a70d4e0badad82663cc6f4bfa
ohno <ddeeppaa.gogo@gmail.com>
2021-04-22 22:38:58 +0900

    ゲストユーザーを削除できないようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root 'pages#index'
   get 'pages/show'
   devise_scope :user do
