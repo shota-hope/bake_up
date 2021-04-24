7228906f0fbb661c60eeecc16daa602dece11d96
ohno <ddeeppaa.gogo@gmail.com>
2021-04-24 09:59:18 +0900

    表示を降順、dependent: :destroy追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
-
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
