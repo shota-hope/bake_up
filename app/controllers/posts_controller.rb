de3f29389cd944eecd08fd14d0e58f6561c1bf63
ohno <ddeeppaa.gogo@gmail.com>
2021-04-29 18:47:48 +0900

    postsコントローラー編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  before_action :sign_in_required, only:[:create, :destroy]
+
+  def create
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :users, :only => [:show]
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  resources :users, only:[:show]
   devise_scope :user do
   post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  resources :posts, only:[:create, :destroy]
 
 end
