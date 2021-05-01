f6cdc222c54d91f33180a59c9d0486c26c4bf92c
ohno <ddeeppaa.gogo@gmail.com>
2021-05-01 16:43:22 +0900

    投稿フォーム作成

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :sign_in_required, only: [:show]
 
   def index
+    @post = current_user.posts.build if user_signed_in?
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ApplicationController
   before_action :sign_in_required, only:[:create, :destroy]
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+       flash[:succuess] = "投稿しました"
+       redirect_to root_url
+    else
+      render 'pages/index'
+    end
   end
 
   def destroy
