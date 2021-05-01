3bf83e78d07d49c8f63f1dfdef25315162061649
ohno <ddeeppaa.gogo@gmail.com>
2021-05-01 18:33:56 +0900

    delete機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   before_action :sign_in_required, only:[:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-       flash[:succuess] = "投稿しました"
+       flash[:success] = "投稿しました"
        redirect_to root_url
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
@@ -13,11 +15,19 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def post_params
       params.require(:post).permit(:content)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
