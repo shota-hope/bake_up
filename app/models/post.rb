f6e7daaecefc9e4008ec3e0282c86d6bdaa2888b
ohno <ddeeppaa.gogo@gmail.com>
2021-05-02 16:53:13 +0900

    画像投稿機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc)}
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum:1000}
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" },
+                      size:         { less_than: 5.megabytes,
+                                      message: "should be less than 5MB" }
+
+  def display_image
+    image.variant(resize_to_limit: [400, 400])
+  end
 end
