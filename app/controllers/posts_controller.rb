6c4ce4c583eb6b682199ccb3b07ce0f5d85473d0
ohno <ddeeppaa.gogo@gmail.com>
2021-05-02 16:00:53 +0900

    画像投稿機能追加（途中）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
+    @post.image.attach(params[:post][:image])
     if @post.save
        flash[:success] = "投稿しました"
        redirect_to root_url
@@ -23,7 +24,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:content, :image)
     end
 
     def correct_user
