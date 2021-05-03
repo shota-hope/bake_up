1e8335ecd9b41de53c5b07b14a503ca49a79dcac
ohno <ddeeppaa.gogo@gmail.com>
2021-05-04 08:23:45 +0900

    Relationshipモデル作成、追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :comments, dependent: :destroy
   validates :username, presence: true
   validates :email, presence: true
   # Include default devise modules. Others available are:
