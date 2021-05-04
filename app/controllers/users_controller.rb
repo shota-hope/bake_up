5ae8546309c15334eef026e7f99b15cd85991510
ohno <ddeeppaa.gogo@gmail.com>
2021-05-04 14:05:11 +0900

    フォロー/フォロワー画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :sign_in_required, only: [:index, :show]
+  before_action :sign_in_required, only: [:index, :show, :following, :followers]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -9,4 +9,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.paginate(page: params[:page])
   end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :users, only:[:index, :show]
+  resources :users, only:[:index, :show] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   devise_scope :user do
   post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
