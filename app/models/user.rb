c808cfdac0c199b7b6d314ee5b03054a6e8b3aa4
ohno <ddeeppaa.gogo@gmail.com>
2021-05-04 09:15:29 +0900

    userモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   has_many :comments, dependent: :destroy
   validates :username, presence: true
   validates :email, presence: true
