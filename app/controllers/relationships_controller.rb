f5d6c779a19adf6f8312bf71081ee3345e302a05
ohno <ddeeppaa.gogo@gmail.com>
2021-05-07 00:42:47 +0900

    feedにフォロワーのpostを表示させる

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -18,5 +18,4 @@ class RelationshipsController < ApplicationController
       format.js
     end
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,10 @@ class User < ApplicationRecord
   end
 
   def feed
-    Post.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする
