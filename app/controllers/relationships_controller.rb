6c01e6f39c3ed6cb1c1d03fbbd1ecd5f4834c8df
ohno <ddeeppaa.gogo@gmail.com>
2021-05-07 22:47:02 +0900

    フォロー、フォロー解除時にフラッシュメッセージ追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,15 +4,18 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
+    flash[:success] = "フォローしました"
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
+
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
+    flash[:success] = "フォロー解除しました"
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
