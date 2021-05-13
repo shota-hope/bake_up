6570813ecd9de9043637ce7ecdc2c031438fe6b4
ohno <ddeeppaa.gogo@gmail.com>
2021-05-13 23:42:30 +0900

    Ajaxによる非同期化

--- app/controllers/favorite_relationships_controller.rb
+++ app/controllers/favorite_relationships_controller.rb
@@ -2,14 +2,22 @@ class FavoriteRelationshipsController < ApplicationController
   before_action :user_signed_in?
 
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
-    redirect_back(fallback_location: root_url)
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_url) }
+      format.js
+    end
   end
 
   def destroy
-    post = FavoriteRelationship.find(params[:id]).post
-    current_user.unlike(post)
-    redirect_back(fallback_location: root_url)
+    @user = current_user
+    @post = FavoriteRelationship.find(params[:id]).post
+    current_user.unlike(@post)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_url) }
+      format.js
+    end
   end
 end
